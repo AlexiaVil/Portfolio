093a50190ba15d3255a11c9bb6f30ce97800758e
Roméo Jr. Labonté <rom@romjim.games>
2024-03-05 12:55:51 -0500

    fix: remove all ls chars

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -47,9 +47,9 @@ export const projects: ProjectData[] = [
 		hero: ["/projects/_placeholder/169.png", "/projects/_placeholder/169.png"],
 		title: "Le français, une langue à célébrer!",
 		description: [
-			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
+			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
 			"En collaboration avec La Bande à Paul, en charge de la direction artistique de l'exposition, CREO avait en charge la direction artistique des huit interactifs.",
-			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
+			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
 		],
 		year: 2023,
 		index: 0,
@@ -72,7 +72,7 @@ export const projects: ProjectData[] = [
 				left: {
 					description: {
 						title: "Un jeu questionnaire - Vite sur tes patins!",
-						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
+						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
 					},
 				},
 				right: {
@@ -93,9 +93,9 @@ export const projects: ProjectData[] = [
 		hero: ["/projects/_placeholder/169.png", "/projects/_placeholder/169.png"],
 		title: "Le français, une langue à célébrer!",
 		description: [
-			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
+			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
 			"En collaboration avec La Bande à Paul, en charge de la direction artistique de l'exposition, CREO avait en charge la direction artistique des huit interactifs.",
-			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
+			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
 		],
 		year: 2023,
 		index: 1,
@@ -109,7 +109,7 @@ export const projects: ProjectData[] = [
 				},
 				right: {
 					description: {
-						title: "Une carte interactive - Accents d'Amérique.",
+						title: "Une carte interactive - Accents d'Amérique",
 						text: "Le but, faire glisser des cartes personnages sur leur provenance géographique en Amérique du Nord, dans un temps imparti.",
 					},
 				},
@@ -118,7 +118,7 @@ export const projects: ProjectData[] = [
 				left: {
 					description: {
 						title: "Un jeu questionnaire - Vite sur tes patins!",
-						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
+						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
 					},
 				},
 				right: {
