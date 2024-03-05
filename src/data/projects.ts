623b5c5e77154819a5be2f7712367467419568cc
Roméo Jr. Labonté <rom@romjim.games>
2024-03-05 12:10:03 -0500

    fix: margin + buttons

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -81,4 +81,50 @@ export const projects: ProjectData[] = [
 			},
 		],
 	},
+	{
+		list: {
+			format: "portrait",
+			title: "Exposition Interactive",
+			description:
+				"Une exposition ludique éphémère sur la langue française au Québec et en Amérique du Nord.",
+			image: "/projects/_placeholder/500.png",
+		},
+		url: "le-francais-une-langue-a-celebrer-2",
+		hero: ["/projects/_placeholder/169.png", "/projects/_placeholder/169.png"],
+		title: "Le français, une langue à célébrer!",
+		description: [
+			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
+			"En collaboration avec La Bande à Paul, en charge de la direction artistique de l'exposition, CREO avait en charge la direction artistique des huit interactifs.",
+			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
+		],
+		year: 2023,
+		index: 1,
+		client: "Pointe-à-Callière, cité d'archéologie et d'histoire de Montréal",
+		collab: "La bande à Paul",
+		tags: ["Exposition Interactive", "Design UI"],
+		screenshots: [
+			{
+				left: {
+					image: "/projects/_placeholder/169.png",
+				},
+				right: {
+					description: {
+						title: "Une carte interactive - Accents d'Amérique.",
+						text: "Le but, faire glisser des cartes personnages sur leur provenance géographique en Amérique du Nord, dans un temps imparti.",
+					},
+				},
+			},
+			{
+				left: {
+					description: {
+						title: "Un jeu questionnaire - Vite sur tes patins!",
+						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
+					},
+				},
+				right: {
+					image: "/projects/_placeholder/169.png",
+				},
+			},
+		],
+	},
 ];
