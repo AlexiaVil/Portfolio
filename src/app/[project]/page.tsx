c0d6e1969cf4772a90e4eb65532f53a4e35c6b26
Roméo Jr. Labonté <rom@romjim.games>
2024-03-05 12:10:53 -0500

    feat: ✨ project page (#6)
    * feat: project page
    * fix: margin + buttons

---
+++ src/app/[project]/page.tsx
@@ -0,0 +1,163 @@
+import Image from "next/image";
+import { ProjectData, projects } from "@/data/projects";
+import styles from "./ProjectPage.module.css";
+import Heading from "@/components/Heading/Heading";
+import Tag from "@/components/Tag/Tag";
+import Button from "@/components/Button/Button";
+
+export default function Page({
+	params: { project },
+}: {
+	params: { project: string };
+}) {
+	const projectData: ProjectData | undefined = projects.find(
+		(p) => p.url === project,
+	);
+
+	if (!projectData) {
+		return <div>Project not found</div>;
+	}
+
+	const previousProject = projects[projectData.index - 1]?.url;
+	const nextProject = projects[projectData.index + 1]?.url;
+
+	console.log(previousProject, nextProject);
+
+	const Hero = ({ className }: { className: string }) => (
+		<div className={[styles.hero, className].join(" ")}>
+			{projectData.hero.map((image) => (
+				<div key={image} className={styles.imageContainer}>
+					<Image
+						alt={projectData.title}
+						fill
+						className={styles.image}
+						src={image}
+					/>
+				</div>
+			))}
+		</div>
+	);
+
+	return (
+		<div className={styles.page}>
+			<Hero className={styles.d} />
+			<div className={styles.content}>
+				<div className={styles.meta}>
+					<Heading variant="h4">{projectData.year}</Heading>
+					<Heading variant="h2" asHeader className={styles.title}>
+						{projectData.title}
+					</Heading>
+					<div className={styles.tags}>
+						{projectData.tags.map((tag) => (
+							<Tag key={tag}>{tag}</Tag>
+						))}
+					</div>
+					<Heading variant="h3">{projectData.client}</Heading>
+					<span className={styles.collab}>
+						en collaboration avec {projectData.collab}
+					</span>
+				</div>
+				<Hero className={styles.m} />
+				<div className={styles.description}>
+					{projectData.description.map((paragraph, i) => (
+						<p key={i}>{paragraph}</p>
+					))}
+				</div>
+			</div>
+			<div className={styles.gallery}>
+				{projectData.screenshots.map((screenshot, i) => {
+					const left = screenshot.left.image ? (
+						<div
+							className={[styles.screenshotImage, styles.screenshotLeft].join(
+								" ",
+							)}
+						>
+							<Image
+								src={screenshot.left.image}
+								fill
+								className={styles.image}
+								alt={""}
+							/>
+						</div>
+					) : (
+						<div
+							className={[styles.screenshotText, styles.screenshotLeft].join(
+								" ",
+							)}
+						>
+							<span className={styles.screenshotTitle}>
+								{screenshot.left.description?.title}
+							</span>
+							<p className={styles.screenshotDescription}>
+								{screenshot.left.description?.text}
+							</p>
+						</div>
+					);
+
+					const right = screenshot.right.image ? (
+						<div
+							className={[styles.screenshotImage, styles.screenshotRight].join(
+								" ",
+							)}
+						>
+							<Image
+								src={screenshot.right.image}
+								fill
+								className={styles.image}
+								alt={""}
+							/>
+						</div>
+					) : (
+						<div
+							className={[styles.screenshotText, styles.screenshotRight].join(
+								" ",
+							)}
+						>
+							<span className={styles.screenshotTitle}>
+								{screenshot.right.description?.title}
+							</span>
+							<p className={styles.screenshotDescription}>
+								{screenshot.right.description?.text}
+							</p>
+						</div>
+					);
+
+					return (
+						<div
+							key={i}
+							className={[
+								styles.screenshotContainer,
+								i % 2 !== 0 ? styles.reverse : "",
+							].join(" ")}
+						>
+							{left}
+							{right}
+						</div>
+					);
+				})}
+			</div>
+			<div className={styles.buttons}>
+				<Button
+					href={previousProject || "#"}
+					disabled={previousProject === undefined}
+					arrow="left"
+				>
+					Précédent
+				</Button>
+				<Button
+					href={nextProject || "#"}
+					disabled={nextProject === undefined}
+					arrow="right"
+				>
+					Suivant
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+export async function generateStaticParams() {
+	return projects.map((project) => ({
+		slug: project.url,
+	}));
+}
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,50 +1,130 @@
-import { ProjectProps } from "@/components/Project/Project";
+export type ProjectFormat = "landscape" | "portrait";
 
-export type ProjectData = Omit<ProjectProps, "align">;
+export interface ProjectData {
+	list: {
+		format: ProjectFormat;
+		title: string;
+		description: string;
+		image: string;
+	};
+	url: string;
+	hero: string[];
+	title: string;
+	description: string[];
+	year: number;
+	index: number;
+	client: string;
+	collab: string;
+	tags: string[];
+	screenshots: {
+		left: {
+			image?: string;
+			description?: {
+				title: string;
+				text: string;
+			};
+		};
+		right: {
+			image?: string;
+			description?: {
+				title: string;
+				text: string;
+			};
+		};
+	}[];
+}
 
 export const projects: ProjectData[] = [
 	{
-		format: "portrait",
-		title: "Exposition Interactive",
-		url: "/exposition-interactive",
-		subtitle: "2023 - Le français, une langue à célébrer!",
-		description:
-			"Une exposition ludique éphémère sur la langue française au Québec et en Amérique du Nord.",
-		image: "/projects/_placeholder/500.png",
+		list: {
+			format: "portrait",
+			title: "Exposition Interactive",
+			description:
+				"Une exposition ludique éphémère sur la langue française au Québec et en Amérique du Nord.",
+			image: "/projects/_placeholder/500.png",
+		},
+		url: "le-francais-une-langue-a-celebrer",
+		hero: ["/projects/_placeholder/169.png", "/projects/_placeholder/169.png"],
+		title: "Le français, une langue à célébrer!",
+		description: [
+			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
+			"En collaboration avec La Bande à Paul, en charge de la direction artistique de l'exposition, CREO avait en charge la direction artistique des huit interactifs.",
+			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
+		],
+		year: 2023,
+		index: 0,
+		client: "Pointe-à-Callière, cité d'archéologie et d'histoire de Montréal",
+		collab: "La bande à Paul",
+		tags: ["Exposition Interactive", "Design UI"],
+		screenshots: [
+			{
+				left: {
+					image: "/projects/_placeholder/169.png",
+				},
+				right: {
+					description: {
+						title: "Une carte interactive - Accents d'Amérique.",
+						text: "Le but, faire glisser des cartes personnages sur leur provenance géographique en Amérique du Nord, dans un temps imparti.",
+					},
+				},
+			},
+			{
+				left: {
+					description: {
+						title: "Un jeu questionnaire - Vite sur tes patins!",
+						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
+					},
+				},
+				right: {
+					image: "/projects/_placeholder/169.png",
+				},
+			},
+		],
 	},
 	{
-		format: "landscape",
-		title: "Exposition Interactive 2",
-		url: "/exposition-interactive",
-		subtitle: "2023 - alerte à l’embâcle!",
-		description: "Apprendre de manière ludique le travail des draveurs.",
-		image: "/projects/_placeholder/500.png",
+		list: {
+			format: "portrait",
+			title: "Exposition Interactive",
+			description:
+				"Une exposition ludique éphémère sur la langue française au Québec et en Amérique du Nord.",
+			image: "/projects/_placeholder/500.png",
+		},
+		url: "le-francais-une-langue-a-celebrer-2",
+		hero: ["/projects/_placeholder/169.png", "/projects/_placeholder/169.png"],
+		title: "Le français, une langue à célébrer!",
+		description: [
+			"Pour le 45ème anniversaire de la Charte de la langue française, le mandat était  de concevoir et réaliser une exposition interactive sur la langue française au Québec et en Amérique du Nord. Le résultat, une exposition ludique, colorée, divisée  en plusieurs zones, avec des vidéos, un jeu de questionnaire, un karaoké et des jeux interactifs numériques.",
+			"En collaboration avec La Bande à Paul, en charge de la direction artistique de l'exposition, CREO avait en charge la direction artistique des huit interactifs.",
+			"Les objectifs : Garantir la cohérence de l'interface avec le contenu et l'univers de l'exposition. Respecter l'identité visuelle de l'exposition (épurée et couleurs vives). Concevoir des interfaces intuitives, attrayante et accessible à un public familiale.",
+		],
+		year: 2023,
+		index: 1,
+		client: "Pointe-à-Callière, cité d'archéologie et d'histoire de Montréal",
+		collab: "La bande à Paul",
+		tags: ["Exposition Interactive", "Design UI"],
+		screenshots: [
+			{
+				left: {
+					image: "/projects/_placeholder/169.png",
+				},
+				right: {
+					description: {
+						title: "Une carte interactive - Accents d'Amérique.",
+						text: "Le but, faire glisser des cartes personnages sur leur provenance géographique en Amérique du Nord, dans un temps imparti.",
+					},
+				},
+			},
+			{
+				left: {
+					description: {
+						title: "Un jeu questionnaire - Vite sur tes patins!",
+						text: "Participer à un grand jeu-questionnaire animé par Katherine Levac et Richardson Zéphir.  La vidéo a été réalisée à l'externe par La Bande à Paul.",
+					},
+				},
+				right: {
+					image: "/projects/_placeholder/169.png",
+				},
+			},
+		],
 	},
-	{
-		format: "landscape",
-		title: "Jeu en ligne multiplateforme",
-		url: "/jeu-en-ligne-multiplateforme",
-		subtitle: "2022 - Carbone Scol'ère",
-		description:
-			"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
-		image: "/projects/_placeholder/500.png",
-	},
-	{
-		format: "portrait",
-		title: "Jeux d'apprentissage",
-		url: "/jeux-d-apprentissage",
-		subtitle: "2021 - axelle et pitchouf : en marche !",
-		description:
-			"Dix segments d'animation interactive et de jeu pour sensibiliser les jeunes à la sécurité routière.",
-		image: "/projects/_placeholder/500.png",
-	},
-	{
-		format: "landscape",
-		title: "Jeu numérique",
-		url: "/jeux-numerique",
-		subtitle: "2020 - Kasskrout",
-		description:
-			"Faire découvrir le Québec et les producteurs, au travers de recette que le joueur doit accomplir en allant chercher des aliments en parcourant une carte des régions.",
-		image: "/projects/_placeholder/500.png",
-	},
-] as const;
+];
