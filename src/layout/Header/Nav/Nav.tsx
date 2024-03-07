d0aea7a1c2de68580b1626cc8594f9137384c2e7
Roméo Jr. Labonté <rom@romjim.games>
2024-03-07 10:27:01 -0500

    fix: 🐛 fix pathname check, remove console.logs

--- src/layout/Header/Nav/Nav.tsx
+++ src/layout/Header/Nav/Nav.tsx
@@ -12,21 +12,16 @@ const Nav = () => {
 	const [scrolling, setScrolling] = useState(false);
 
 	useEffect(() => {
-		console.log("effect running");
 		if (destination === 0) return;
 		if (scrolling) return;
 
-		console.log("scrolling to", destination);
-		console.log("scrollY", window.scrollY);
 		const controls = animate(window.scrollY, destination, {
 			ease: [0.16, 1, 0.3, 1],
 			duration: 0.5,
 			onUpdate: (value) => {
-				console.log("  value", value);
 				window.scrollTo(0, value);
 			},
 			onComplete: () => {
-				console.log("scroll complete");
 				setScrolling(false);
 				setDestination(0);
 			},
@@ -34,17 +29,12 @@ const Nav = () => {
 
 		setScrolling(true);
 		return () => {
-			console.log("cleanup");
 			controls.stop();
 		};
 	}, [destination]);
 
-	const isHome = pathname === `${process.env.basePath}/`;
+	const isHome = pathname === "/";
 	const getLink = (href: string) => {
-		console.log("isHome", isHome);
-		console.log("  pathname", pathname);
-		console.log("  basePath", process.env.basePath);
-		console.log("  href", href);
 		return isHome ? href : `${process.env.basePath}/${href}`;
 	};
 
@@ -54,7 +44,6 @@ const Nav = () => {
 		e.preventDefault();
 
 		const target = e.currentTarget.getAttribute("data-target");
-		console.log("target", target);
 		if (!target) return;
 
 		const targetElement = Array.from(
@@ -62,15 +51,12 @@ const Nav = () => {
 				.querySelectorAll<HTMLElement>(`section[data-section=${target}]`)
 				.values(),
 		).filter((e) => window.getComputedStyle(e).display !== "none")[0];
-		console.log("targetElement", targetElement);
 		if (!targetElement) return;
 
 		const body = document.querySelector("body");
-		console.log("body", body);
 		if (!body) return;
 
 		const html = document.querySelector("html");
-		console.log("html", html);
 		if (!html) return;
 
 		const height =
@@ -82,14 +68,12 @@ const Nav = () => {
 				html.offsetHeight,
 			) - window.innerHeight;
 
-		console.log("height", height);
 		const offset =
 			targetElement.getBoundingClientRect().top -
 			body.offsetTop +
 			window.scrollY -
 			130;
 
-		console.log("offset", offset);
 		setDestination(Math.min(offset, height));
 	};
 
